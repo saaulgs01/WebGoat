2c29f58785dfe00fda1cb4a75f2fc3c0c9b3c54f
saaulgs01 <91549407+saaulgs01@users.noreply.github.com>
2023-04-16 16:42:50 +0200

    Update SqlInjectionLesson3.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson3.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson3.java
@@ -55,30 +55,30 @@ public class SqlInjectionLesson3 extends AssignmentEndpoint {
   }
 
   protected AttackResult injectableQuery(String query) {
-    try (Connection connection = dataSource.getConnection()) {
-      try (Statement statement =
-          connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)) {
-        Statement checkStatement =
-            connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
-        statement.executeUpdate(query);
-        ResultSet results =
-            checkStatement.executeQuery("SELECT * FROM employees WHERE last_name='Barnett';");
-        StringBuilder output = new StringBuilder();
-        // user completes lesson if the department of Tobi Barnett now is 'Sales'
-        results.first();
-        if (results.getString("department").equals("Sales")) {
-          output.append("<span class='feedback-positive'>" + query + "</span>");
-          output.append(SqlInjectionLesson8.generateTable(results));
-          return success(this).output(output.toString()).build();
-        } else {
-          return failed(this).output(output.toString()).build();
-        }
-
-      } catch (SQLException sqle) {
-        return failed(this).output(sqle.getMessage()).build();
+  try (Connection connection = dataSource.getConnection()) {
+    try (PreparedStatement statement = connection.prepareStatement(
+        "UPDATE employees SET department=? WHERE last_name=?")) {
+      // Establecer los parámetros de la consulta
+      statement.setString(1, "Sales");
+      statement.setString(2, "Barnett");
+      statement.executeUpdate();
+      ResultSet results = statement.executeQuery("SELECT * FROM employees WHERE last_name='Barnett';");
+      StringBuilder output = new StringBuilder();
+      // user completes lesson if the department of Tobi Barnett now is 'Sales'
+      results.first();
+      if (results.getString("department").equals("Sales")) {
+        output.append("<span class='feedback-positive'>" + query + "</span>");
+        output.append(SqlInjectionLesson8.generateTable(results));
+        return success(this).output(output.toString()).build();
+      } else {
+        return failed(this).output(output.toString()).build();
       }
-    } catch (Exception e) {
-      return failed(this).output(this.getClass().getName() + " : " + e.getMessage()).build();
+
+    } catch (SQLException sqle) {
+      return failed(this).output(sqle.getMessage()).build();
     }
+  } catch (Exception e) {
+    return failed(this).output(this.getClass().getName() + " : " + e.getMessage()).build();
   }
 }
+
