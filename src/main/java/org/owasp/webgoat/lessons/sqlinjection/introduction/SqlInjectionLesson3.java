566e514883c0425a1ba7885bd8bb30c4396a97cc
saaulgs01 <91549407+saaulgs01@users.noreply.github.com>
2023-04-16 17:12:24 +0200

    Update SqlInjectionLesson3.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson3.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson3.java
@@ -60,8 +60,7 @@ public class SqlInjectionLesson3 extends AssignmentEndpoint {
           connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)) {
         Statement checkStatement =
             connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
-        PreparedStatment ps = connection.prepareStatment(query);
-        ps.executeUpdate();
+//        statement.executeUpdate(query);
             
 //        statement.executeUpdate(query);
         ResultSet results =
